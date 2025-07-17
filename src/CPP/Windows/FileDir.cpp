ff8fb5107b33f01f8b834f332c74e103b1203e7c
Vladyslav Starina <vstarina@readdle.com>
2025-07-17 16:12:44 +0200

    Revert "fixed static-analysis"
    This reverts commit 0f37b4b362be380b6091dd729901e19354006c3b.

--- src/CPP/Windows/FileDir.cpp
+++ src/CPP/Windows/FileDir.cpp
@@ -1014,20 +1014,15 @@ bool GetCurrentDir(FString &path)
   {
     // if (errno != ERANGE) return false;
     #if defined(__GLIBC__) || defined(__APPLE__)
-      long size = pathconf(".", _PC_PATH_MAX);
-      if (size == -1) size = 4096;
-
-      char *pathBuffer = (char *)malloc(size);
-      if (!pathBuffer) return false;
-
-      res = getcwd(pathBuffer, size);
+    /* As an extension to the POSIX.1-2001 standard, glibc's getcwd()
+       allocates the buffer dynamically using malloc(3) if buf is NULL. */
+    res = getcwd(NULL, 0);
     if (res)
     {
-      path = fas2fs(pathBuffer);
-      ::free(pathBuffer);
+      path = fas2fs(res);
+      ::free(res);
       return true;
     }
-      ::free(pathBuffer);
     #endif
     return false;
   }
